72e3a280af56ae8ee107bcdf128e5ad3fccbfef5
Peter Palaga <peter@palaga.org>
2026-09-15 16:24:38 +0000

    Test org.l2x6.discotheq.api.DiscoClient.majorVersions()

--- api/src/main/java/org/l2x6/discotheq/api/DiscoClient.java
+++ api/src/main/java/org/l2x6/discotheq/api/DiscoClient.java
@@ -1,9 +1,8 @@
 package org.l2x6.discotheq.api;
 
 import io.quarkus.rest.client.reactive.QuarkusRestClientBuilder;
-import org.l2x6.discotheq.api.builder.MajorVersionsBuilder;
-
 import java.net.URI;
+import org.l2x6.discotheq.api.builder.MajorVersionsBuilder;
 
 public class DiscoClient {
 
@@ -13,12 +12,14 @@ public class DiscoClient {
 
     public static DiscoClient of(String baseUri) {
         return new DiscoClient(QuarkusRestClientBuilder.newBuilder()
-            .baseUri(URI.create(baseUri))
-            .build(DiscoApi.class));
+                .baseUri(URI.create(baseUri))
+                .build(DiscoApi.class));
     }
+
     public DiscoClient(DiscoApi api) {
         this.api = api;
     }
+
     private final DiscoApi api;
 
     public MajorVersionsBuilder majorVersions() {
--- api/src/main/java/org/l2x6/discotheq/api/builder/MajorVersionsBuilder.java
+++ api/src/main/java/org/l2x6/discotheq/api/builder/MajorVersionsBuilder.java
@@ -1,15 +1,14 @@
 package org.l2x6.discotheq.api.builder;
 
 import io.smallrye.mutiny.Uni;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.l2x6.discotheq.api.DiscoApi;
 import org.l2x6.discotheq.api.model.ApiResponse;
 import org.l2x6.discotheq.api.model.DiscoveryScope;
 import org.l2x6.discotheq.api.model.MajorVersion;
 import org.l2x6.discotheq.api.model.Match;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 public class MajorVersionsBuilder {
     private final DiscoApi api;
@@ -35,10 +34,12 @@ public class MajorVersionsBuilder {
         this.ea = ea;
         return this;
     }
+
     public MajorVersionsBuilder ea() {
         this.ea = true;
         return this;
     }
+
     public MajorVersionsBuilder notEa() {
         this.ea = false;
         return this;
@@ -103,14 +104,17 @@ public class MajorVersionsBuilder {
         this.includeVersions = false;
         return this;
     }
+
     public MajorVersionsBuilder matchAll() {
         this.match = Match.ALL;
         return this;
     }
+
     public MajorVersionsBuilder matchAny() {
         this.match = Match.ANY;
         return this;
     }
+
     public MajorVersionsBuilder discoveryScopeId(DiscoveryScope... discoveryScopeIds) {
         if (discoveryScopeIds != null && discoveryScopeIds.length > 0) {
             if (this.discoveryScopeId == null) {
@@ -122,10 +126,11 @@ public class MajorVersionsBuilder {
         }
         return this;
     }
+
     public MajorVersionsBuilder discoveryScopeId(Collection<DiscoveryScope> discoveryScopeIds) {
         if (discoveryScopeIds != null && discoveryScopeIds.size() > 0) {
             if (this.discoveryScopeId == null) {
-                this.discoveryScopeId = new ArrayList<>(discoveryScopeIds.length);
+                this.discoveryScopeId = new ArrayList<>(discoveryScopeIds.size());
             }
             this.discoveryScopeId.addAll(discoveryScopeIds);
         }
